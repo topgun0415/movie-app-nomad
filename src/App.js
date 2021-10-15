f76bcf23f8f26c01de067e8c33f25b12e86b61de
philipuuu <topgun0415@naver.com>
2021-10-15 20:32:18 +0900

    Test : react pprops test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+/* eslint-disable no-unused-vars */
+
+function Food({ favorite }) {
+  console.log();
+  return <h3>I like {favorite}</h3>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello React!!!</h1>
+      <Food favorites='SUSHI' />
+      <Food favorites='SAMGEOPSAL' />
     </div>
   );
 }
