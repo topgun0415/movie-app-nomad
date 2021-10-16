1f8c6ae50f6a6369cbac954c8269529149bfe8bd
philipuuu <topgun0415@naver.com>
2021-10-16 18:45:05 +0900

    Test : React Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
 /** @format */
-import PropTypes from 'prop-types';
+import React from 'react';
 
-// foodILike function
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimbab',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/5/56/Vegetable_gimbap.jpg',
-    rating: '4.7 / 5.0',
-  },
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-  {
-    id: 2,
-    name: 'Bbokki',
-    image: 'https://themoonworld.com/wp-content/uploads/2020/12/DSC08076.jpg',
-    rating: '4.2 / 5.0',
-  },
-];
+  Add = () => {
+    console.log('add');
+    this.setState((current) => ({ count: this.current.count + 1 }));
+  };
+  Subtract = () => {
+    console.log('Subtract');
+    this.setState((current) => ({ count: this.current.count - 1 }));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
-};
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <img src={picture} alt=''></img>
-      <h4>Rating : {rating}</h4>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.Add}>Add</button>
+        <button onClick={this.Subtract}>Subtract</button>
+      </div>
+    );
+  }
 }
 
 export default App;
-/* renderfood
-function renderfood(dish) {
-  console.log(dish);
-  return <Food name={dish.name} picture={dish.image} />;
-}
-*/
