04f802689b229213012a0079a8a871e8696c7b5d
philipuuu <topgun0415@naver.com>
2021-10-16 16:05:19 +0900

    Test : React map-recap, PPropTypes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
 /** @format */
-/* eslint-disable no-unused-vars */
+import PropTypes from 'prop-types';
 
-function Food({ name, pics }) {
+// foodILike function
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimbab',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/5/56/Vegetable_gimbap.jpg',
+    rating: '4.7 / 5.0',
+  },
+
+  {
+    id: 2,
+    name: 'Bbokki',
+    image: 'https://themoonworld.com/wp-content/uploads/2020/12/DSC08076.jpg',
+    rating: '4.2 / 5.0',
+  },
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+};
+
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h3>I like {name}</h3>
-      <img src={pics}></img>
+      <img src={picture} alt=''></img>
+      <h4>Rating : {rating}</h4>
     </div>
   );
 }
-
-const foodILike = [
-  {
-    name: 'Kimbab',
-    Star: 5,
-    image: 'https://en.wikipedia.org/wiki/File:Vegetable_gimbap.jpg',
-  },
-
-  {
-    name: 'Bbokki',
-    Star: 5,
-    image: 'https://themoonworld.com/wp-content/uploads/2020/12/DSC08076.jpg',
-  },
-];
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} pics={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
 }
 
 export default App;
+/* renderfood
+function renderfood(dish) {
+  console.log(dish);
+  return <Food name={dish.name} picture={dish.image} />;
+}
+*/
