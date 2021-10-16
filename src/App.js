d04b6219ac0a4c41eb2dc51d621d1758f7be26c9
philipuuu <topgun0415@naver.com>
2021-10-16 14:19:13 +0900

    Test : React Dynamic-Component-Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 /** @format */
 /* eslint-disable no-unused-vars */
 
-function Food({ favorite }) {
-  console.log();
-  return <h3>I like {favorite}</h3>;
+function Food({ name, pics }) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img src={pics}></img>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'Kimbab',
+    Star: 5,
+    image: 'https://en.wikipedia.org/wiki/File:Vegetable_gimbap.jpg',
+  },
+
+  {
+    name: 'Bbokki',
+    Star: 5,
+    image: 'https://themoonworld.com/wp-content/uploads/2020/12/DSC08076.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello React!!!</h1>
-      <Food favorites='SUSHI' />
-      <Food favorites='SAMGEOPSAL' />
+    <div>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} pics={dish.image} />
+      ))}
     </div>
   );
 }
